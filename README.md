545d03bb58f21aa75f73d08158d9af5f81a45387
utkarsh-hhh <utkarshv.brainerhub@gmail.com>
2025-10-28 12:41:02 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# this is the simple github action demo
---
+++ test/test_opration.py
@@ -0,0 +1,10 @@
+from src.math_operation import add , sub
+
+
+def test_add():
+    assert add(2,3)==5
+    assert add(-1,1)==0
+
+def test_sub():
+    assert sub(5,3)==3
+    assert sub(4,3)==1
